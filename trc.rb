fdae162ecb1e2eaf9e3513c4a2c31b3adecf0da3
ka <ka.kaosf@gmail.com>
2014-05-28 23:29:09 +0900

    Use Readline library

--- trc.rb
+++ trc.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'twitter'
+require 'readline'
 
 $VERBOSE = nil # not to output any warning to stderr
 
@@ -11,10 +12,4 @@ Twitter.configure do |config|
   config.oauth_token_secret = 'YOUR ORIGINAL'
 end
 
-if ARGV.length == 0
-  Twitter.update(STDIN.gets)
-else
-  if ARGV[0] == '-t' && ARGV.length >= 2
-    Twitter.update(ARGV[1])
-  end
-end
+Twitter.update(Readline.readline)
